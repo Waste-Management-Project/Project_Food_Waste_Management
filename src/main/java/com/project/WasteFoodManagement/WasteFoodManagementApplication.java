4eabf4fd7f74a658d7e72303a2d52772f8487079
sankalptrimade <sankalptrimade0014@gmail.com>
2023-03-25 12:27:48 +0530

    Project

---
+++ src/main/java/com/project/WasteFoodManagement/WasteFoodManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.WasteFoodManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteFoodManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteFoodManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/WasteFoodManagement/WasteFoodManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.WasteFoodManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteFoodManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
